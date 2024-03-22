e0ad101c625018976118580997d825f2dbadc2aa
brandon-grossman <125304905+brandon-grossman@users.noreply.github.com>
2024-03-22 08:10:01 -0500

    .

---
+++ GiphyAPI/Models/GifRating.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace GiphyAPI.Models
+{
+    public class GifRating
+    {
+        public string? gif_id { get; set; }
+        public int rating { get; set; }
+        public IdentityUser? user { get; set; }
+    }
+}
